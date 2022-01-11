e52b376cb391f8f8d661cc5c3c948c1a102abfc2
Witktor Pink <Wiktor.Pink@automaton.ovh>
2022-01-11 02:21:25 +0100

    Google sign in troubles

--- app/src/main/java/com/example/remedy/MainActivity.kt
+++ app/src/main/java/com/example/remedy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.remedy
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
@@ -18,6 +19,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -42,6 +46,9 @@ private val RC_SIGN_IN = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
         FirebaseAuth.getInstance().useEmulator("10.0.2.2", 9099);
         //auth = Firebase.aith
         val database = Firebase.database
@@ -69,7 +76,7 @@ private val RC_SIGN_IN = 9001
 
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("YOUR_WEB_APPLICATION_CLIENT_ID")
+            .requestIdToken("156991665561-76cqnq8mrppicoug9em5lunbgbb1d1gc.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
@@ -84,6 +91,23 @@ private val RC_SIGN_IN = 9001
 
   }
 
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if(user == null)
+        {
+            Log.w(TAG, "User is null, net going to navigate")
+            return
+        }
+        startActivity(Intent(this, CarScanActivity::class.java))
+        finish()
+    }
+
     private fun signIn() {
         val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(
@@ -92,10 +116,19 @@ private val RC_SIGN_IN = 9001
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            val task =
-                GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
         }
     }
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -129,6 +162,24 @@ private val RC_SIGN_IN = 9001
             )
         }
     }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
     private fun signOut() {
         mGoogleSignInClient.signOut()
             .addOnCompleteListener(this) {
